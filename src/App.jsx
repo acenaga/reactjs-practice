13ea2cde6f93409e9f7017844241d1cd2263dd3d
Carlos Ferrer <mundofuturoca@gmail.com>
2025-09-11 22:15:13 -0300

    added styledComponents and button component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import ToggleButton from "./components/ToggleButton"
 import NameForm from "./components/NameForm"
 import CounterWithEffect from "./components/CounterWithEffect"
 import Card from "./components/Card/Card"
+import Button from "./components/Buttons/Button"
 function App() {
 
   return (
     <>
       <NameForm />
+      <Button blue>Primary Button</Button>
+      <Button>Secondary Button</Button>
       <Counter />
       <ToggleButton />
       <CounterWithEffect />
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,17 @@
 import styles from './card.module.scss';
 
 const Card = () => {
+    const textStyle = {
+        boxShadow: '2px 2px 4px #000000',
+    }
     return(
         <div className={styles.card}>
             <h2>Hola soy una Card</h2>
             <p>Este es el contenido de la Card</p>
+            <p style={{ textStroke: '1px darkblue', WebkitTextStroke: '1px darkblue' }}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem assumenda odio enim, quasi dicta adipisci iste laudantium dolorem nobis a mollitia, dolorum exercitationem quas laborum consectetur optio eligendi id saepe!</p>
+            <figure style={textStyle}>
+                <img src="/assets/css.png" alt="soy el logo de css3" />
+            </figure>
         </div>
     );
 };
